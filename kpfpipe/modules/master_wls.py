87dd0549b00d5c860ebc75e5e61622eb0d7acc16
Greg Gilbert <gjgilbert@astro.ucla.edu>
2026-02-14 08:29:24 -0800

    add outlines of a few class structures

---
+++ kpfpipe/modules/master_wls.py
@@ -0,0 +1,6 @@
+from kpfpipe.modules.image_assembly import ImageAssembly
+from kpfpipe.modules.image_processing import ImageProcessing
+
+class WLS:
+    def __init__(self, l0_obj_list):
+        pass
